853620819063f4cbe224987ea5f95edd48915e82
Muhammad Abdullah Khalid <48885205+makhalid1999@users.noreply.github.com>
2023-11-27 23:43:40 +0500

    Update common.py

--- bm/models/common.py
+++ bm/models/common.py
@@ -5,421 +5,69 @@
 # LICENSE file in the root directory of this source tree.
 
 from functools import partial
-import logging
-import math
+import random
 import typing as tp
 
-import mne
 import torch
 from torch import nn
+from torch.nn import functional as F
+import torchaudio as ta
+import numpy as np
+from collections import OrderedDict
 
-from ..studies.api import Recording
-
-
-logger = logging.getLogger(__name__)
-
-
-def pad_multiple(x: torch.Tensor, base: int):
-    length = x.shape[-1]
-    target = math.ceil(length / base) * base
-    return torch.nn.functional.pad(x, (0, target - length))
+from .common import (
+    ConvSequence, ScaledEmbedding, SubjectLayers,
+    DualPathRNN, ChannelMerger, ChannelDropout, pad_multiple, SpatialAttention, SubjectLayer
+)
 
 
-class ScaledEmbedding(nn.Module):
-    """Scale up learning rate for the embedding, otherwise, it can move too slowly.
-    """
-    def __init__(self, num_embeddings: int, embedding_dim: int, scale: float = 10.):
-        super().__init__()
-        self.embedding = nn.Embedding(num_embeddings, embedding_dim)
-        self.embedding.weight.data /= scale
-        self.scale = scale
-
-    @property
-    def weight(self):
-        return self.embedding.weight * self.scale
-
-    def forward(self, x):
-        return self.embedding(x) * self.scale
-
-
-class SubjectLayers(nn.Module):
-    """Per subject linear layer."""
-    def __init__(self, in_channels: int, out_channels: int, n_subjects: int, init_id: bool = False):
-        super().__init__()
-        self.weights = nn.Parameter(torch.randn(n_subjects, in_channels, out_channels))
-        if init_id:
-            assert in_channels == out_channels
-            self.weights.data[:] = torch.eye(in_channels)[None]
-        self.weights.data *= 1 / in_channels**0.5
-
-    def forward(self, x, subjects):
-        _, C, D = self.weights.shape
-        weights = self.weights.gather(0, subjects.view(-1, 1, 1).expand(-1, C, D))
-        return torch.einsum("bct,bcd->bdt", x, weights)
-
-    def __repr__(self):
-        S, C, D = self.weights.shape
-        return f"SubjectLayers({C}, {D}, {S})"
-
-
-class LayerScale(nn.Module):
-    """Layer scale from [Touvron et al 2021] (https://arxiv.org/pdf/2103.17239.pdf).
-    This rescales diagonaly residual outputs close to 0 initially, then learnt.
-    """
-    def __init__(self, channels: int, init: float = 0.1, boost: float = 5.):
-        super().__init__()
-        self.scale = nn.Parameter(torch.zeros(channels, requires_grad=True))
-        self.scale.data[:] = init / boost
-        self.boost = boost
-
-    def forward(self, x):
-        return (self.boost * self.scale[:, None]) * x
-
-
-class ConvSequence(nn.Module):
-
-    def __init__(self, channels: tp.Sequence[int], kernel: int = 4, dilation_growth: int = 1,
-                 dilation_period: tp.Optional[int] = None, stride: int = 2,
-                 dropout: float = 0.0, leakiness: float = 0.0, groups: int = 1,
-                 decode: bool = False, batch_norm: bool = False, dropout_input: float = 0,
-                 skip: bool = False, scale: tp.Optional[float] = None, rewrite: bool = False,
-                 activation_on_last: bool = True, post_skip: bool = False, glu: int = 0,
-                 glu_context: int = 0, glu_glu: bool = True, activation: tp.Any = None) -> None:
-        super().__init__()
-        dilation = 1
-        channels = tuple(channels)
-        self.skip = skip
-        self.sequence = nn.ModuleList()
-        self.glus = nn.ModuleList()
-        if activation is None:
-            activation = partial(nn.LeakyReLU, leakiness)
-        Conv = nn.Conv1d if not decode else nn.ConvTranspose1d
-        # build layers
-        for k, (chin, chout) in enumerate(zip(channels[:-1], channels[1:])):
-            layers: tp.List[nn.Module] = []
-            is_last = k == len(channels) - 2
-
-            # Set dropout for the input of the conv sequence if defined
-            if k == 0 and dropout_input:
-                assert 0 < dropout_input < 1
-                layers.append(nn.Dropout(dropout_input))
-
-            # conv layer
-            if dilation_growth > 1:
-                assert kernel % 2 != 0, "Supports only odd kernel with dilation for now"
-            if dilation_period and (k % dilation_period) == 0:
-                dilation = 1
-            pad = kernel // 2 * dilation
-            layers.append(Conv(chin, chout, kernel, stride, pad,
-                               dilation=dilation, groups=groups if k > 0 else 1))
-            dilation *= dilation_growth
-            # non-linearity
-            if activation_on_last or not is_last:
-                if batch_norm:
-                    layers.append(nn.BatchNorm1d(num_features=chout))
-                layers.append(activation())
-                if dropout:
-                    layers.append(nn.Dropout(dropout))
-                if rewrite:
-                    layers += [nn.Conv1d(chout, chout, 1), nn.LeakyReLU(leakiness)]
-                    # layers += [nn.Conv1d(chout, 2 * chout, 1), nn.GLU(dim=1)]
-            if chin == chout and skip:
-                if scale is not None:
-                    layers.append(LayerScale(chout, scale))
-                if post_skip:
-                    layers.append(Conv(chout, chout, 1, groups=chout, bias=False))
-
-            self.sequence.append(nn.Sequential(*layers))
-            if glu and (k + 1) % glu == 0:
-                ch = 2 * chout if glu_glu else chout
-                act = nn.GLU(dim=1) if glu_glu else activation()
-                self.glus.append(
-                    nn.Sequential(
-                        nn.Conv1d(chout, ch, 1 + 2 * glu_context, padding=glu_context), act))
-            else:
-                self.glus.append(None)
-
-    def forward(self, x: tp.Any) -> tp.Any:
-        for module_idx, module in enumerate(self.sequence):
-            old_x = x
-            x = module(x)
-            if self.skip and x.shape == old_x.shape:
-                x = x + old_x
-            glu = self.glus[module_idx]
-            if glu is not None:
-                x = glu(x)
-        return x
-
-
-class DualPathRNN(nn.Module):
-    def __init__(self, channels: int, depth: int, inner_length: int = 10):
-        super().__init__()
-        self.lstms = nn.ModuleList([nn.LSTM(channels, channels, 1) for _ in range(depth * 4)])
-        self.inner_length = inner_length
-
-    def forward(self, x: torch.Tensor):
-        B, C, L = x.shape
-        IL = self.inner_length
-        x = pad_multiple(x, self.inner_length)
-        x = x.permute(2, 0, 1).contiguous()
-        for idx, lstm in enumerate(self.lstms):
-            y = x.reshape(-1, IL, B, C)
-            if idx % 2 == 0:
-                y = y.transpose(0, 1).reshape(IL, -1, C)
-            else:
-                y = y.reshape(-1, IL * B, C)
-            y, _ = lstm(x)
-            if idx % 2 == 0:
-                y = y.reshape(IL, -1, B, C).transpose(0, 1).reshape(-1, B, C)
-            else:
-                y = y.reshape(-1, B, C)
-            x = x + y
-
-            if idx % 2 == 1:
-                x = x.flip(dims=(0,))
-        return x[:L].permute(1, 2, 0).contiguous()
-
-
-class PositionGetter:
-    INVALID = -0.1
-
-    def __init__(self) -> None:
-        self._cache: tp.Dict[int, torch.Tensor] = {}
-        self._invalid_names: tp.Set[str] = set()
-
-    def get_recording_layout(self, recording: Recording) -> torch.Tensor:
-        index = recording.recording_index
-        if index in self._cache:
-            return self._cache[index]
-        else:
-            info = recording.mne_info
-            layout = mne.find_layout(info)
-            indexes: tp.List[int] = []
-            valid_indexes: tp.List[int] = []
-            for meg_index, name in enumerate(info.ch_names):
-                name = name.rsplit("-", 1)[0]
-                try:
-                    indexes.append(layout.names.index(name))
-                except ValueError:
-                    if name not in self._invalid_names:
-                        logger.warning(
-                            "Channels %s not in layout for recording %s of %s.",
-                            name,
-                            recording.study_name(),
-                            recording.recording_uid)
-                        self._invalid_names.add(name)
-                else:
-                    valid_indexes.append(meg_index)
-
-            positions = torch.full((len(info.ch_names), 2), self.INVALID)
-            x, y = layout.pos[indexes, :2].T
-            x = (x - x.min()) / (x.max() - x.min())
-            y = (y - y.min()) / (y.max() - y.min())
-            x = torch.from_numpy(x).float()
-            y = torch.from_numpy(y).float()
-            positions[valid_indexes, 0] = x
-            positions[valid_indexes, 1] = y
-            self._cache[index] = positions
-            return positions
-
-    def get_positions(self, batch):
-        meg = batch.meg
-        B, C, T = meg.shape
-        positions = torch.full((B, C, 2), self.INVALID, device=meg.device)
-        for idx in range(len(batch)):
-            recording = batch._recordings[idx]
-            rec_pos = self.get_recording_layout(recording)
-            positions[idx, :len(rec_pos)] = rec_pos.to(meg.device)
-        return positions
-
-    def is_invalid(self, positions):
-        return (positions == self.INVALID).all(dim=-1)
-
-
-class FourierEmb(nn.Module):
-    """
-    Fourier positional embedding.
-    Unlike trad. embedding this is not using exponential periods
-    for cosines and sinuses, but typical `2 pi k` which can represent
-    any function over [0, 1]. As this function would be necessarily periodic,
-    we take a bit of margin and do over [-0.2, 1.2].
-    """
-    def __init__(self, dimension: int = 256, margin: float = 0.2):
-        super().__init__()
-        n_freqs = (dimension // 2)**0.5
-        assert int(n_freqs ** 2 * 2) == dimension
-        self.dimension = dimension
-        self.margin = margin
-
-    def forward(self, positions):
-        *O, D = positions.shape
-        assert D == 2
-        *O, D = positions.shape
-        n_freqs = (self.dimension // 2)**0.5
-        freqs_y = torch.arange(n_freqs).to(positions)
-        freqs_x = freqs_y[:, None]
-        width = 1 + 2 * self.margin
-        positions = positions + self.margin
-        p_x = 2 * math.pi * freqs_x / width
-        p_y = 2 * math.pi * freqs_y / width
-        positions = positions[..., None, None, :]
-        loc = (positions[..., 0] * p_x + positions[..., 1] * p_y).view(*O, -1)
-        emb = torch.cat([
-            torch.cos(loc),
-            torch.sin(loc),
-        ], dim=-1)
-        return emb
-
-
-class ChannelDropout(nn.Module):
-    def __init__(self, dropout: float = 0.1, rescale: bool = True):
-        """
-        Args:
-            dropout: dropout radius in normalized [0, 1] coordinates.
-            rescale: at valid, rescale all channels.
-        """
-        super().__init__()
-        self.dropout = dropout
-        self.rescale = rescale
-        self.position_getter = PositionGetter()
-
-    def forward(self, meg, batch):
-        if not self.dropout:
-            return meg
-
-        B, C, T = meg.shape
-        meg = meg.clone()
-        positions = self.position_getter.get_positions(batch)
-        valid = (~self.position_getter.is_invalid(positions)).float()
-        meg = meg * valid[:, :, None]
-
-        if self.training:
-            center_to_ban = torch.rand(2, device=meg.device)
-            kept = (positions - center_to_ban).norm(dim=-1) > self.dropout
-            meg = meg * kept.float()[:, :, None]
-            if self.rescale:
-                proba_kept = torch.zeros(B, C, device=meg.device)
-                n_tests = 100
-                for _ in range(n_tests):
-                    center_to_ban = torch.rand(2, device=meg.device)
-                    kept = (positions - center_to_ban).norm(dim=-1) > self.dropout
-                    proba_kept += kept.float() / n_tests
-                meg = meg / (1e-8 + proba_kept[:, :, None])
-
-        return meg
-
-
-class ChannelMerger(nn.Module):
-    def __init__(self, chout: int, pos_dim: int = 256,
-                 dropout: float = 0, usage_penalty: float = 0.,
-                 n_subjects: int = 200, per_subject: bool = False):
-        super().__init__()
-        assert pos_dim % 4 == 0
-        self.position_getter = PositionGetter()
-        self.per_subject = per_subject
-        if self.per_subject:
-            self.heads = nn.Parameter(torch.randn(n_subjects, chout, pos_dim, requires_grad=True))
-        else:
-            self.heads = nn.Parameter(torch.randn(chout, pos_dim, requires_grad=True))
-        self.heads.data /= pos_dim ** 0.5
-        self.dropout = dropout
-        self.embedding = FourierEmb(pos_dim)
-        self.usage_penalty = usage_penalty
-        self._penalty = torch.tensor(0.)
-
-    @property
-    def training_penalty(self):
-        return self._penalty.to(next(self.parameters()).device)
-
-    def forward(self, meg, batch):
-        B, C, T = meg.shape
-        meg = meg.clone()
-        positions = self.position_getter.get_positions(batch)
-        embedding = self.embedding(positions)
-        score_offset = torch.zeros(B, C, device=meg.device)
-        score_offset[self.position_getter.is_invalid(positions)] = float('-inf')
-
-        if self.training and self.dropout:
-            center_to_ban = torch.rand(2, device=meg.device)
-            radius_to_ban = self.dropout
-            banned = (positions - center_to_ban).norm(dim=-1) <= radius_to_ban
-            score_offset[banned] = float('-inf')
-
-        if self.per_subject:
-            _, cout, pos_dim = self.heads.shape
-            subject = batch.subject_index
-            heads = self.heads.gather(0, subject.view(-1, 1, 1).expand(-1, cout, pos_dim))
-        else:
-            heads = self.heads[None].expand(B, -1, -1)
-
-        scores = torch.einsum("bcd,bod->boc", embedding, heads)
-        scores += score_offset[:, None]
-        weights = torch.softmax(scores, dim=2)
-        out = torch.einsum("bct,boc->bot", meg, weights)
-        if self.training and self.usage_penalty > 0.:
-            usage = weights.mean(dim=(0, 1)).sum()
-            self._penalty = self.usage_penalty * usage
-        return out
-
-class SubjectLayer(nn.Module):
-  def __init__(self, outchans, n_subjects):
+class SimpleConv(nn.Module):   
+  def __init__(self, in_channels, out_channels, n_subjects):
     super().__init__()
-    self.subj_layers = nn.Sequential(*[nn.Conv2d(outchans, outchans, 1, padding='same') for i in range(n_subjects)])
+    F_out = out_channels
+    inchans = in_channels
+    self.D2 = 320
+    self.outchans = 270
+    self.spatial_attention = SpatialAttention(inchans, 270, 32)
+    self.conv = nn.Conv2d(270, 270, 1, padding='same')
+    self.subject_layer = SubjectLayer(270, n_subjects)
+    self.conv_blocks = nn.Sequential(*[self.generate_conv_block(k) for k in range(5)]) # 5 conv blocks
+    self.final_convs = nn.Sequential(
+      nn.Conv2d(self.D2, self.D2*2, 1),
+      nn.GELU(),
+      nn.Conv2d(self.D2*2, F_out, 1)
+    )
+    
+  def generate_conv_block(self, k):
+    kernel_size = (1,3)
+    padding = 'same' # (p,0)
+    return nn.Sequential(OrderedDict([
+      ('conv1', nn.Conv2d(self.outchans if k==0 else self.D2, self.D2, kernel_size, dilation=pow(2,(2*k)%5), padding=padding)),
+      ('bn1',   nn.BatchNorm2d(self.D2)), 
+      ('gelu1', nn.GELU()),
+      ('conv2', nn.Conv2d(self.D2, self.D2, kernel_size, dilation=pow(2,(2*k+1)%5), padding=padding)),
+      ('bn2',   nn.BatchNorm2d(self.D2)),
+      ('gelu2', nn.GELU()),
+      ('conv3', nn.Conv2d(self.D2, self.D2*2, kernel_size, padding=padding)),
+      ('glu',   nn.GLU(dim=1))
+    ]))
 
-  def forward(self, x, subj_indices):
-    for i in range(x.shape[0]):
-      x[i] = self.subj_layers[subj_indices[i] - 1](x[i].clone())
-    return x
+  def forward(self, x, batch):
+    subjects = batch.subject_index
+    x = self.spatial_attention(x, batch).unsqueeze(2) # add dummy dimension at the end
+    x = self.conv(x)
+    x = self.subject_layer(x, subjects)
         
-class SpatialAttention(nn.Module):
-  def __init__(self,in_channels, out_channels, K):
-    super().__init__()
-    self.outchans = out_channels
-    self.inchans = in_channels
-    self.K = K
-    self.x = x.to(device=device)
-    self.y = y.to(device=device)
-    self.x_drop = random.uniform(0, 1)
-    self.y_drop = random.uniform(0, 1)
-    self.compute_cos_sin()           
-    # trainable parameter:
-    self.z = Parameter(torch.randn(self.outchans, K*K, dtype = torch.cfloat,device=device)/(32*32)) # each output channel has its own KxK z matrix
-    self.z.requires_grad = True
-
-def get_positions(self, batch):
-    meg = batch.meg
-    B, C, T = meg.shape
-    positions = torch.full((B, C, 2), self.INVALID, device=meg.device)
-    for idx in range(len(batch)):
-        recording = batch._recordings[idx]
-        rec_pos = self.get_recording_layout(recording)
-        positions[idx, :len(rec_pos)] = rec_pos.to(meg.device)
-    return positions
-
-  def forward(self, X, batch):
-    positions = self.position_getter.get_positions(batch)
-    self.x = positions[:,0]
-    self.y = positions[:,1]
-    kk = torch.arange(1, self.K+1, device=device)
-    ll = torch.arange(1, self.K+1, device=device)
-    cos_fun = lambda k, l, x, y: torch.cos(2*torch.pi*(k*x + l*y))
-    sin_fun = lambda k, l, x, y: torch.sin(2*torch.pi*(k*x + l*y))
-    self.cos_matrix = torch.stack([cos_fun(kk[None,:], ll[:,None], x, y) for x, y in zip(self.x, self.y)]).reshape(self.inchans,-1).float()
-    self.sin_matrix = torch.stack([sin_fun(kk[None,:], ll[:,None], x, y) for x, y in zip(self.x, self.y)]).reshape(self.inchans,-1).float()
-    a = torch.matmul(self.z.real, self.cos_matrix.T) + torch.matmul(self.z.imag, self.sin_matrix.T)
-    # Question: divide this with square root of KxK? to stablize gradient as with self-attention?
-    for i in range(self.inchans):
-      distance = (self.x_drop - self.x[i])**2 + (self.y_drop - self.y[i])**2
-      if distance < 0.1:
-        a[:, i] = 0
+    for k in range(len(self.conv_blocks)):
+      if k == 0:
+        x = self.conv_blocks[k](x)
+      else:
+        x_copy = x
+        for name, module in self.conv_blocks[k].named_modules():
+          if name == 'conv2' or name == 'conv3':
+            x = x_copy + x # residual skip connection for the first two convs
+            x_copy = x.clone() # is it deep copy?
+          x = module(x)
+    x = self.final_convs(x)
         
-    a = F.softmax(a, dim=1) # softmax over all input chan location for each output chan
-                                            # outchans x  inchans
-                
-            # X: N x 273 x 360            
-    X = torch.matmul(a, X) # N x outchans x 360 (time)
-                                   # matmul dim expansion logic: https://pytorch.org/docs/stable/generated/torch.matmul.html
-    return X
-
-
-
+    return x.squeeze(2)
