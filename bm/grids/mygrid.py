a2446a480a1bc8d3f5bd6963d00b591a701f0df8
Muhammad Abdullah Khalid <48885205+makhalid1999@users.noreply.github.com>
2023-12-06 14:00:40 +0500

    Update mygrid.py

--- bm/grids/mygrid.py
+++ bm/grids/mygrid.py
@@ -6,7 +6,7 @@ from ._explorers import ClipExplorer
 def explorer(launcher):
 	# Methods `slurm_` and `bind_` are in-place.
     launcher.slurm_(gpus=4)  # All XPs scheduled with `launcher` will use 4 gpus.
-    launcher.bind_({'norm.max_scale': 20, 'dset.n_recordings': 2})  # set common params.
+    launcher.bind_({'norm.max_scale': 20, 'dset.n_recordings': 15})  # set common params.
 
     more_subjects = {'dset.n_recordings': 100}
     selections = ['brennan2019']
